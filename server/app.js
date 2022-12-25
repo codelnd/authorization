e89cbcd868d7abd5babad1198ec761656873753a
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-25 22:40:23 +0700

    feat: создать структуру проекта

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const PORT = 3200
+const app = express()
+
+const startServer = async () => {
+    try {
+        app.listen(PORT, () => console.log(`Сервер запущен на ${PORT} порту`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+startServer()
+
+
