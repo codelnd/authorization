dd4f615dfa2e28ddd796ac571ffde0ccc3eb4736
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-26 22:22:06 +0700

    feat: прописать коннект к бд и прописать мидлвэры

--- server/app.js
+++ server/app.js
@@ -2,11 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const PORT = process.env.PORT || 3200
-const app = express()
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3200;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+
 
 const startServer = async () => {
     try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         app.listen(PORT, () => console.log(`Сервер запущен на ${PORT} порту`))
     } catch (err) {
         console.log(err)
