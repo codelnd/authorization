049bc251e1b9d8d4e5a2cbb349208263960815ed
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-28 21:37:35 +0700

    feat: импортировать роутер

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const router = require('./routes/index');
 
 const PORT = process.env.PORT || 3200;
 const app = express();
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-
+app.use('/api', router);
 
 
 const startServer = async () => {
