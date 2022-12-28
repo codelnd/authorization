30cf08581e4b328d513ae6c2c71be2bd434bb448
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-28 21:49:53 +0700

    feat: прописать контроллеры в роуты

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,11 @@ const userController = require('../controllers/user-controller');
 const router = new Router();
 
 router.get('/users', userController.getUsers);
-router.post('/register');
-router.post('/login');
-router.post('/logout');
-router.get('/activate/:link'); // Активация аккаунта по ссылке на почту
-router.get('/refresh'); // Перезапись access токена
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate); // Активация аккаунта по ссылке на почту
+router.get('/refresh', userController.refresh); // Перезапись access токена
 
 
 module.exports = router;
