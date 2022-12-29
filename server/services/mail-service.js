8586d0ae98046ecfb491e6da47e95bf543f4dc1e
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-30 01:57:49 +0700

    feat: создать контроллер отправки письма на почту

---
+++ server/services/mail-service.js
@@ -0,0 +1,5 @@
+class MailService {
+    async sendActivationLink(to, link) {}
+}
+
+module.exports = new MailService()
--- server/services/user-service.js
+++ server/services/user-service.js
@@ -1,6 +1,7 @@
 const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
+const mailService = require('./mail-service')
 
 class UserService {
     async register(email, password) {
@@ -11,6 +12,7 @@ class UserService {
             const hashPassword = await bcrypt.hash(password, 4);
             const activationLink = uuid.v4();
             const newUser = await UserModel.create({email, password: hashPassword, activationLink})
+            await mailService.sendActivationLink(email, activationLink)
         }
     }
 }
