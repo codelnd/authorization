8f0261b63952c282d960ec3b2758b7c87dfa5280
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-30 02:22:15 +0700

    feat: создать функцию генерации пары токенов

---
+++ server/services/token-service.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+
+class TokenService {
+    generateTokens(payload) {
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '30m'})
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
+
+        return {
+            accessToken,
+            refreshToken
+        }
+    }
+}
+
+module.exports = new TokenService()
