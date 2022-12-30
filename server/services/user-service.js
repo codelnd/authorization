d570abca2ce7b5febb29f85850ce02ff6e2f71f9
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-30 21:11:35 +0700

    feat: передать dto в функцию генерации токена

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -1,20 +1,22 @@
-const userModel = require('../models/user-model');
+const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
 const tokenService = require('./token-service');
+const UserDto = require('../dtos/user-dto');
 
 class UserService {
     async register(email, password) {
-        const registeredUser = await userModel.findOne({email})
+        const registeredUser = await UserModel.findOne({email})
         if (registeredUser) {
             throw new Error(`Пользователь с почтой ${registeredUser} уже зарегистрирован`)
         } else {
             const hashPassword = await bcrypt.hash(password, 4);
             const activationLink = uuid.v4();
-            const newUser = await userModel.create({email, password: hashPassword, activationLink})
+            const newUser = await UserModel.create({email, password: hashPassword, activationLink})
             await mailService.sendActivationLink(email, activationLink)
-            const tokens = tokenService.generateTokens()
+            const userDto = new UserDto(newUser);
+            const tokens = tokenService.generateTokens({...userDto})
         }
     }
 }
