73d151d4d026069db49a36c5391b2b7193b3f6d4
Denis Bezukladnikov <denis3094@mail.ru>
2023-02-24 23:11:49 +0700

    feat: создать mailservice

--- server/services/mail-service.js
+++ server/services/mail-service.js
@@ -2,9 +2,16 @@ const nodemailer = require('nodemailer');
 
 class MailService {
     constructor() {
-
+        this.transporter = nodemailer.createTransport({
+            host: '',
+            port: '',
+            secure: false,
+            auth: {}
+        })
     }
-    async sendActivationLink(to, link) {}
+
+    async sendActivationLink(to, link) {
+    }
 }
 
 module.exports = new MailService()
